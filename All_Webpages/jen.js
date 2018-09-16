a697c5812ce1d4d90563794e533551f83318ac03
jennifer bernal <jennifer.bernal@ufl.edu>
2018-09-16 01:54:16 -0400

    night end

--- All_Webpages/jen.js
+++ All_Webpages/jen.js
@@ -56,36 +56,36 @@
         {
             //only function that needs to be called, rest do it "automatically"
             function assignVars() {
-                var vf = document.getElementById("finalVelocity").value;
-                var vi = document.getElementById("initialVelocity").value;
-                var a = document.getElementById("acceleration").value;
-                var t = document.getElementById("time").value;
+                var vf2 = document.getElementById("finalV2").value;
+                var vi2 = document.getElementById("initialV2").value;
+                var a2 = document.getElementById("acc2").value;
+                var t2 = document.getElementById("time2").value;
 
-                document.getElementById("vfVal").innerHTML = vf;
-                document.getElementById("viVal").innerHTML = vi;
-                document.getElementById("aVal").innerHTML = a;
-                document.getElementById("tVal").innerHTML = t;
+                document.getElementById("vfVal2").innerHTML = vf2;
+                document.getElementById("viVal2").innerHTML = vi2;
+                document.getElementById("aVal2").innerHTML = a2;
+                document.getElementById("tVa2").innerHTML = t2;
 
-                checkInputs(vf, vi, a, t);
+                checkInputs(vf2, vi2, a2, t2);
             }
-            function checkInputs(vf, vi, a, t) {
-                var answer;
-                if (vf == "") {
-                    answer = vi + a * t;
+            function checkInputs(vf2, vi2, a2, t2) {
+                var ans2;
+                if (vf2 == "") {
+                    ans2 = vi2 + a2 * t2;
                 }
-                else if (vi == "") {
-                    answer = vf - a * t;
+                else if (vi2 == "") {
+                    ans2 = vf2 - a2 * t2;
                 }
-                else if (a == "") {
-                    answer = (1.0/t) * (vf-vi);
+                else if (a2 == "") {
+                    ans2 = (1.0/t2) * (vf2-vi2);
                 }
-                else if (t == "") {
-                    answer = (1.0/a) * (vf-vi);        
+                else if (t2 == "") {
+                    ans2 = (1.0/a2) * (vf2-vi2);        
                 }
                 else {
                     //document.write("Please fill in 3 fields.");
                 }
-                document.getElementById("ans").innerHTML = answer;
+                document.getElementById("answer2").innerHTML = ans2;
             }
             return assignVars();
         }
